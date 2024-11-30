8e4576ad2d67db84567bcb3e87bf42fc175e733f
HelenaMadrenys <hmadrenys@gmail.com>
2024-11-30 17:23:03 +0100

    Fixed building3 failing and added different threshold velocities

--- Assets/Scripts/Controllers/BuildingController.cs
+++ Assets/Scripts/Controllers/BuildingController.cs
@@ -6,6 +6,14 @@ public class BuildingController : MonoBehaviour
 {
     [SerializeField]
     private bool m_isRandomized;
+    [SerializeField]
+    private float m_velocity1;
+    [SerializeField]
+    private float m_velocity2;
+    [SerializeField]
+    private float m_velocity3;
+    [SerializeField]
+    private float m_velocity4;
 
     [Header("Meshes")]
     [SerializeField]
@@ -21,13 +29,12 @@ public class BuildingController : MonoBehaviour
     [SerializeField]
     private Mesh m_buildingDestroyed;
 
-    [SerializeField]
-    private LayerMask m_layerMask;
     private Collider m_collider;
     private MeshFilter m_meshFilter;
 
     private int m_height = 0;
     private int m_rotation = 0;
+    private float m_breakVelocity;
 
     private void Awake()
     {
@@ -40,30 +47,36 @@ public class BuildingController : MonoBehaviour
             m_rotation = Random.Range(0, 4);
         }
 
-        SetMesh();
+        SetBuilding();
     }
 
-    private void SetMesh()
+    private void SetBuilding()
     {
         switch (m_height)
         {
             case 0:
                 m_meshFilter.mesh = m_flat_concrete;
-                m_collider.enabled = false;
                 break;
             case 1:
                 m_meshFilter.mesh = m_building1;
+                m_breakVelocity = m_velocity1;
                 break;
             case 2:
                 m_meshFilter.sharedMesh = m_building2;
+                m_breakVelocity = m_velocity2;
                 break;
             case 3:
                 m_meshFilter.sharedMesh = m_building3;
+                m_breakVelocity = m_velocity3;
                 break;
             case 4:
                 m_meshFilter.sharedMesh = m_building4;
+                m_breakVelocity = m_velocity4;
                 break;
         }
+
+        if (m_height!=0)
+            m_collider.enabled = true;
 
         switch (m_rotation)
         {
@@ -86,7 +99,7 @@ public class BuildingController : MonoBehaviour
     {
         PlayerController playerController = other.gameObject.GetComponentInParent<PlayerController>();
 
-        if (playerController.GetVelocity().magnitude <=500)
+        if (playerController.GetVelocity().magnitude <= m_breakVelocity)
         {
             playerController.Bounce(transform.position);
         }
