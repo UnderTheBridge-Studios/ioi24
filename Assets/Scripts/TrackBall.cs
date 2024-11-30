124044d8598b77aab0841bf9e5de134961fb02a4
pereruiz29 <65725598+pereruiz29@users.noreply.github.com>
2024-12-01 00:07:41 +0100

    Hamster follow input

---
+++ Assets/Scripts/TrackBall.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using DG.Tweening;
+
+public class TrackBall : MonoBehaviour
+{
+    private Transform m_ball;
+    private Vector2 m_input;
+    private float m_angle;
+    [SerializeField] private float m_lerpVelocity = 0.2f;
+
+    private void Start()
+    {
+        m_ball = GameManager.Instance.Player1.transform;
+    }
+
+    void Update()
+    {
+        transform.position = m_ball.position;
+        transform.DOLocalRotate(new Vector3(0, m_angle, 0), 0.3f);
+        Debug.Log("transform.eulerAngles: " + transform.eulerAngles + " m_angle: " + m_angle);
+    }
+
+    public void Rotate(InputAction.CallbackContext context)
+    {
+        m_input = context.ReadValue<Vector2>();
+
+        m_angle = Vector2.SignedAngle(m_input, Vector2.up);
+    }
+
+}
