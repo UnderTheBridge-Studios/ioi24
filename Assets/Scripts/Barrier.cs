23b2e83f89901af09a344c5d14fbadee046faa64
pereruiz29 <65725598+pereruiz29@users.noreply.github.com>
2024-12-01 13:27:41 +0100

    barrier v2

--- Assets/Scripts/Barrier.cs
+++ Assets/Scripts/Barrier.cs
@@ -4,20 +4,22 @@ using DG.Tweening;
 
 public class Barrier : MonoBehaviour
 {
-
     private int nPlayers = 0;
     [SerializeField] private GameObject m_barrierMesh;
+    [SerializeField] private GameObject m_barrierMesh2;
 
     private void OnTriggerEnter(Collider other)
     {
         nPlayers++;
-        m_barrierMesh.transform.DOLocalRotate(new Vector3(40, 45, 0), 0.2f);
+        m_barrierMesh2.transform.DOLocalMoveY(0, 0.2f);
+        //m_barrierMesh.transform.DOLocalRotate(new Vector3(40, 45, 0), 0.2f);
     }
 
     private void OnTriggerExit(Collider other)
     {
         nPlayers--;
         if(!(nPlayers>0))
-            m_barrierMesh.transform.DOLocalRotate(new Vector3(0, 45, 0), 0.2f);
+            m_barrierMesh2.transform.DOLocalMoveY(0, 0.2f);
+            //m_barrierMesh.transform.DOLocalRotate(new Vector3(0, 45, 0), 0.2f);
     }
 }
